5d7c5dd9e50a4b30754611c1bf5ad494f8aac4b3
merttapik <merttapik@hotmail.com>
2022-12-06 16:28:50 +0300

    first

--- src/nft-market.ts
+++ src/nft-market.ts
@@ -1,92 +1,21 @@
-import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  NFTTransfer as NFTTransferEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Transfer as TransferEvent
-} from "../generated/NFTMarket/NFTMarket"
-import {
-  Approval,
-  ApprovalForAll,
-  NFTTransfer,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+import {NFTMarket, NFTTransfer as NFTTransferEvent,} from "../generated/NFTMarket/NFTMarket"
+import { NFT } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleNFTTransfer(event: NFTTransferEvent): void {
-  let entity = new NFTTransfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenID = event.params.tokenID
+  let entity = new NFT(event.params.tokenID.toString());
+  
   entity.from = event.params.from
   entity.to = event.params.to
-  entity.tokenURI = event.params.tokenURI
   entity.price = event.params.price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  const nftMarket=NFTMarket.bind(event.address);
+  const tokenURI=nftMarket.tokenURI(event.params.tokenID);
+  entity.tokenURI=tokenURI;
 
   entity.save()
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
-  entity.save()
-}
+
